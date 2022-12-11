6eb74d20a81217b3fe03d071296003fe6c3918b5
Yasmin <yazzmin0011@gmail.com>
2022-12-11 20:16:40 +0200

    CHANGE THE APP TO REMINDER ONLY

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import ReminderList from "./screens/ReminderList";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import TodoList from "./screens/TodoList";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ModifyReminder from "./screens/ModifyReminder";
 import { useEffect, useState } from "react";
@@ -88,50 +87,12 @@ export default function App() {
       <StatusBar style="light" />
       <NavigationContainer>
         <HandleNotifications />
-        <BottomTabs.Navigator
-          screenOptions={{
-            tabBarStyle: {
-              backgroundColor: GlobalStyles.colors.purple700,
-            },
-            tabBarActiveTintColor: "white",
-            tabBarInactiveTintColor: GlobalStyles.colors.purple100,
-          }}
-        >
-          <BottomTabs.Screen
-            name="RemindersOverview"
-            component={RemindersOverview}
-            options={{
-              headerShown: false,
-              tabBarLabel: "Reminders",
-              tabBarIcon: ({ color, size }) => (
-                <MaterialCommunityIcons
-                  name="reminder"
-                  color={color}
-                  size={size}
-                />
-              ),
-            }}
+   
+          <RemindersOverview
+      
           />
-          <BottomTabs.Screen
-            name="TodoList"
-            component={TodoList}
-            options={{
-              headerStyle: {
-                backgroundColor: GlobalStyles.colors.purple700,
-              },
-              headerTintColor: "white",
-              title: "To Do List",
-              tabBarLabel: "To Do",
-              tabBarIcon: ({ color, size }) => (
-                <MaterialCommunityIcons
-                  name="format-list-checks"
-                  color={color}
-                  size={size}
-                />
-              ),
-            }}
-          />
-        </BottomTabs.Navigator>
+         
+        
       </NavigationContainer>
     </>
   );
